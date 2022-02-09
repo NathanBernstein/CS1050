105b81b91a1e263a98ba1e68cf0f41a746ee580f
nbernstein3 <nbernst3@msudenver.edu>
2022-02-09 13:45:01 -0700

    updated Feb9.java

--- Feb9.java
+++ Feb9.java
@@ -1,10 +1,14 @@
 public class Feb9{
   public static void main(String[] args){
+    //practicing if and if else
     int x,y;
     x=0;
     y=10;
     if(x>y){
       System.out.println("X greater than Y");
+    }
+    else if(x==y){
+      System.out.println("X equals Y");
     }
     else{
       System.out.println("Y greater than X");
