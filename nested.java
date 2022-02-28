6fcc7a0dae4eceac099eeae244dca6e5fd74cfa3
nbernstein3 <nbernst3@msudenver.edu>
2022-02-28 13:15:02 -0700

    updated nested.java

--- nested.java
+++ nested.java
@@ -13,5 +13,23 @@ public class nested{
       }
       System.out.println();
     }
+    for (int i = 1; i <= r; ++i) {
+      for (int v = 0; v<i;v++){
+        System.out.print(" ");
+      }
+      for (int j = 5; j >= i; --j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+    for (int i = 1; i <= r; ++i) {
+      for (int v = 6; v>i;--v){
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
   }
 }
