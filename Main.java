09122ccd99674e3777c5935d15d90b87bc59a0d0
nbernstein3 <nbernst3@msudenver.edu>
2022-04-28 15:34:35 -0600

    Updated final project for CS1050

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@
 // @Class:  CS1050 Sec2
 // @Email:  nbernst3@msudenver.edu
 
-//import ___________
+import java.util.ArrayList;
 class Student{
   //define class variables name,email and studentnumber
-  private String name;
-  private String email;
-  private int stnum;
+  String name;
+  String email;
+  int stnum;
   public Student(String name,String email,int stnum) {
     // initialize class variables
     this.name = name;
@@ -25,9 +25,9 @@ class Student{
 }
 class Book{
     //define class variables bookid,bookname,author
-    private int bookid;
-    private String name;
-    private String author;
+    int bookid;
+    String name;
+    String author;
     public Book(int bookid,String name,String author) {
       // initialize class variables
       this.bookid = bookid;
@@ -54,7 +54,7 @@ class Library{
     if(email.indexOf(".")==-1){
       return -1;
     }
-    for(i=email.indexOf(".");i<email.length();i++){
+    for(int i=email.indexOf(".");i<email.length();i++){
       hasNecc+=1;
     }
     if(hasNecc==3){
@@ -67,19 +67,37 @@ class Library{
   }
   public void deletemember(String name) {
     //accept name in parameter remove member from list
+    for(int i =0;i<roster.size();i++){
+      if(roster.get(i).name==name){
+        roster.remove(i);
+      }
+    }
   }
   public void addbook(int bookid,String name,String author){
     //accept book details in parameter and add book to list
+    catalog.add(new Book(bookid, name, author));
   }
   public int searchbook(String name) {
     //accept bookname in parameter, search and return index if found
+    for(int i =0;i<catalog.size();i++){
+      if(catalog.get(i).name==name){
+        return i;
+      }
+    }
+    return -1;
     // return index
   }
   public void lendbook(String name) {
     //accept the book to lend as parameter and remove from list
+    for(int i =0;i<catalog.size();i++){
+      if(catalog.get(i).name==name){
+        catalog.remove(i);
+      }
+    }
   }
-  public void returnbook(String name) {
+  public void returnbook(int bookid,String name,String author) {
     //accept the book to return as parameter and add back to list
+    catalog.add(new Book(bookid, name, author));
   }
   public void prnStudents(){
     System.out.println(roster);
@@ -88,18 +106,25 @@ class Library{
     System.out.println(roster);//print book catalog
   }
 }
-public class ProgProj
+public class Main
 {
    // main method for testing
    public static void main(String[] args)
    {
      Library MSULibrary=new Library();
      int done= MSULibrary.addmember("Jeff", "jeff@gmail.com", 789012);
+     int done1= MSULibrary.addmember("Jeffery", "jeffery@gmail.com", 123456);
+     int done2= MSULibrary.addmember("Bobby", "bobby@gmail.com", 573824);
+     int done3= MSULibrary.addmember("Bob", "bob@gmail.com", 924832);
      // add few more members
      MSULibrary.deletemember ("Jeff");
      MSULibrary.addbook(1, "GRIT","Duckworth");
+     MSULibrary.addbook(2, "Ender's Game","Card");
+     MSULibrary.addbook(3, "The Odyssey","Homer");
+     MSULibrary.addbook(4, "The Iliad","Homer");
      // add few more books
-     int num= MSULibrary.searchbook(“GRIT”);
+     int num= MSULibrary.searchbook("GRIT");
+     System.out.println("Search for 'GRIT' gives: "+ num);
      // if search is successful call and test lendbook method
 
      //call and test returnbook method
